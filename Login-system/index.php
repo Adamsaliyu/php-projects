63985b32b498d465ceb20433daba2ef50c95699c
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-16 17:40:51 +0500

    adding styles

---
+++ Login-system/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login-system</title>
+</head>
+<body>
+    <h1>HELLO</h1>
+    
+</body>
+</html>
+
+<?php
+    echo "HII";
+?>
