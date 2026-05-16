beef130b507e487069760c7722aedde8850763c3
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-16 18:00:59 +0500

    Building the fundamentals

--- Login-system/index.php
+++ Login-system/index.php
@@ -7,7 +7,26 @@
     <title>login-system</title>
 </head>
 <body>
-    <h1>HELLO</h1>
+    <div class="container">
+        <div class="mid-container">
+            <form action="index.php" method="post">
+                <label for="username">Username</label><br>
+                <input type="text" name="username">
+                <br>
+                <label for="Email">Email</label><br>
+                <input type="email" name="Email">
+                <br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password">
+                <br>
+                <label for="cornfirm-password">Cornfirm password</label><br>
+                <input type="password" name="cornfirm-password"><br>
+
+                <input type="submit" name="Sign up" id="Submit-btn">
+
+            </form>
+        </div>
+    </div>
     
 </body>
 </html>
