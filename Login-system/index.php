23db33f1a7295c97fa706fdf963c2098a50bd703
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-16 22:29:41 +0500

    updating the files

--- Login-system/index.php
+++ Login-system/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style .css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login-system</title>
 </head>
@@ -13,14 +13,9 @@
                 <label for="username">Username</label><br>
                 <input type="text" name="username">
                 <br>
-                <label for="Email">Email</label><br>
-                <input type="email" name="Email">
-                <br>
                 <label for="password">Password</label><br>
                 <input type="password" name="password">
                 <br>
-                <label for="cornfirm-password">Cornfirm password</label><br>
-                <input type="password" name="cornfirm-password"><br>
 
                 <input type="submit" name="Sign up" id="Submit-btn">
 
@@ -34,10 +29,15 @@
 <?php
 
     $name = $_POST["username"];
+    $userName = "muhammad";
+    $userPassword = 11122;
 
 
     if(empty($_POST["username"])){
-        echo"Enter your name";
+        echo"Fields cannot be empty";
+    }
+
+    if(isset( $name )){
 
     }
 ?>
