ea6b93d975c183bda1193a0b6fefd23bd5461b4c
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-20 18:11:26 +0500

    Building dashboard and design

--- Login-system/dashboard.php
+++ Login-system/dashboard.php
@@ -2,10 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>dashboard-page</title>
 </head>
 <body>
+    <div class="dashbord-container">
+        <div class="header">
+            <div class="left-header">
+                 <img src="" alt="">
+                <h3>NETMOVIE</h3>
+            </div>
+            <div class="right-header">
+                <a href="Home">Home</a>
+                <a href="About">About</a>
+            </div>
+
+        </div>
+        <div class="hero">
+
+        </div>
+    </div>
     
 </body>
 </html>
