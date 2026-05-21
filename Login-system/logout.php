e755b71afe0343a890b90eae565da4894b246c13
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-21 17:25:43 +0500

    Updating text in logout

--- Login-system/logout.php
+++ Login-system/logout.php
@@ -2,10 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log out</title>
 </head>
 <body>
+    <div class="container">
+        <h1>LOGGED OUT</h1>
+
+    </div>
     
 </body>
 </html>
+
+<?php
+    echo"LOG OUT";
+?>
